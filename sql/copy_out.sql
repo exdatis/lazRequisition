57d402cf1f09474e71a17ab01e2264c94ce6ca4c
exdatis <exdatis@gmail.com>
2015-04-06 13:10:10 +0200

    CopyOut finished.

---
+++ sql/copy_out.sql
@@ -0,0 +1,79 @@
+﻿create or replace function copy_out(order_id integer, operater integer) returns integer as
+$body$
+declare this_date date;
+declare this_time varchar(30);
+declare new_id integer;
+declare this_doc_id integer;
+declare original_rec record;
+declare items_rec record;
+declare order_as_string varchar(15);
+begin
+  order_as_string = cast($1 as varchar(15));
+  /* select current date */
+  select current_date into this_date;
+  /* select current time */
+  select cast(current_time as varchar(30)) into this_time;
+  /* doc id */
+  this_doc_id = 3; /* interna prijemnica */
+  /* new id */
+  select nextval('mu_nalog_mu_id_seq') into new_id;
+
+  /* select sorages from order */
+  for original_rec in
+  (select
+    a.mi_magacin,
+    a.mi_out
+   from
+     mi_nalog a
+   where
+     a.mi_id = $1)
+   loop
+	INSERT INTO 
+	  mu_nalog(mu_id, 
+		   mu_doc, 
+		   mu_datum, 
+		   mu_magacin, 
+		   mu_notes, 
+		   mu_out, 
+		   mu_storno, 
+		   mu_evident, 
+		   mu_operater, 
+		   mu_time, 
+		   mu_osnov)
+	    VALUES (new_id, 
+		    this_doc_id, 
+		    this_date, 
+		    original_rec.mi_out, 
+		    'Generisana prijemnica', 
+		    original_rec.mi_magacin, 
+		    'Ne', 
+		    'Ne', 
+		    $2, 
+		    this_time, 
+		    order_as_string);
+   end loop;
+   /* selektuj stavke i prenesi */
+   for items_rec in
+   (select
+     b.mis_artikal,
+     b.mis_kolicina
+    from
+      mi_stavke b
+    where
+      b.mis_veza = $1)
+    loop
+	INSERT INTO 
+	  mu_stavke(mus_artikal, 
+		    mus_kolicina, 
+		    mus_veza)
+	    VALUES (items_rec.mis_artikal,
+		    items_rec.mis_kolicina,
+		    new_id);
+    end loop;
+    /* return new id */
+    return new_id;
+end;
+
+$body$
+  LANGUAGE plpgsql
+      
--- storage/ustoragein.pas
+++ storage/ustoragein.pas
@@ -36,11 +36,13 @@ type
     Panel3: TPanel;
     tsItems: TTabSheet;
     tsOrder: TTabSheet;
+    procedure btnGenAllClick(Sender: TObject);
     procedure dbgOrderMouseMove(Sender: TObject; Shift: TShiftState; X,
       Y: Integer);
     procedure dbgOrderTitleClick(Column: TColumn);
   private
     { private declarations }
+    procedure genInput;
   public
     { public declarations }
     procedure doNewRec; override;
@@ -62,7 +64,7 @@ const
 
 implementation
 uses
-  udbm, uexdatis;
+  udbm, uexdatis, uopendatasets;
 {$R *.lfm}
 
 { TfrmStorageIn }
@@ -74,10 +76,112 @@ begin
   Application.ProcessMessages;
 end;
 
+procedure TfrmStorageIn.btnGenAllClick(Sender: TObject);
+begin
+  // check storages
+  if MessageDlg('ExDatis', 'Proverili ste magacin prijema?', mtConfirmation, [mbYes, mbNo], 0) = mrYes then
+    genInput;
+end;
+
 procedure TfrmStorageIn.dbgOrderTitleClick(Column: TColumn);
 begin
   if not dbm.qStorageIn.IsEmpty then
     dbm.qStorageIn.IndexFieldNames:= Column.FieldName;
+  Application.ProcessMessages;
+end;
+
+procedure TfrmStorageIn.genInput;
+var
+  thisOrder : Integer = -1;
+  thisUser : Integer = -1;
+  orderAsString : String = '';
+  sql : String;
+  new_id : Integer = -1;
+  error_msg, successMsgText : String;
+  newDialog : TdlgOpenDataSets;
+begin
+  orderAsString:= InputBox('Broj predatnice', 'Unesite broj predatnice:', '');
+  orderAsString:= StringReplace(orderAsString, #13, '', [rfReplaceAll]);
+  try
+    thisOrder:= StrToInt(orderAsString);
+  except
+    on e : Exception do
+    begin
+      ShowMessage(e.Message);
+      Exit;
+    end;
+  end;
+  //if ok, get user-id
+  thisUser:= getUserId;
+  //run query
+  Screen.Cursor:= crSQLWait;
+  // prepare sql
+  dbm.qGeneral.Close;
+  dbm.qGeneral.SQL.Clear;
+
+  sql:= 'SELECT * FROM copy_out(' + IntToStr(thisOrder) + ', ' + IntToStr(thisUser) + ')';
+  //debug msg
+  //ShowMessage(sql);
+  dbm.qGeneral.SQL.Text:= sql;
+  try
+    dbm.qGeneral.Open;
+    //check new rec
+    if dbm.qGeneral.IsEmpty then
+      begin
+        error_msg:= 'Nepoznata greška.' + #13#10;
+        error_msg:= error_msg + 'Nemojte pokušavati ponovo.' + #13#10;
+        error_msg:= error_msg + 'Pozovite: 062-804-8432' + #13#10;
+        Screen.Cursor:= crDefault;
+        ShowMessage(error_msg);
+        Exit;
+      end;
+    new_id:= dbm.qGeneral.Fields[0].AsInteger;
+    dbm.qGeneral.ApplyUpdates;
+    dbm.dbt.CommitRetaining;
+  except
+    on e : Exception do
+    begin
+      dbm.qGeneral.CancelUpdates;
+      dbm.dbt.RollbackRetaining;
+      Screen.Cursor:= crDefault;
+      ShowMessage(e.Message);
+      Exit;
+    end;
+  end;
+
+  //else reopen order
+  dbm.qStorageIn.DisableControls;
+  //cretate dialog
+  newDialog:= TdlgOpenDataSets.Create(nil);
+  //dbm.qStorageIn.Close;
+  try
+    dbm.dbh.CloseDataSets;
+    dbm.dbh.Close();
+    dbm.dbh.Open;
+    //osvezi sve za probu
+    //dbm.qStorageIn.Open;
+    newDialog.openDataSets;
+  except
+    on e : Exception do
+    begin
+      newDialog.Free;
+      Screen.Cursor:= crDefault;
+      ShowMessage(e.Message);
+      dbm.qStorageIn.EnableControls;
+      Exit;
+    end;
+  end;
+  //free dialog
+  newDialog.Free;
+  //locate
+  if dbm.qStorageIn.Locate('mu_id', new_id, []) then
+    dbm.qStorageIn.EnableControls
+  else
+    dbm.qStorageIn.EnableControls;
+  //set cursor
+  Screen.Cursor:= crDefault;
+  successMsgText:= 'Kreirana je prijemnica broj: ' + IntToStr(new_id);
+  ShowMessage(successMsgText);
   Application.ProcessMessages;
 end;
 
