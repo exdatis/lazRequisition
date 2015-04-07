6430bdfd792a18828ae995455c8476da02a34a20
exdatis <exdatis@gmail.com>
2015-04-07 10:02:10 +0200

    qItems finished.

---
+++ sql/vstoragein_items.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE VIEW vstoragein_items AS 
+SELECT 
+  a.mus_id,
+  a.mus_artikal,
+  a.mus_kolicina,
+  a.mus_veza,
+  b.art_sifra,
+  b.art_naziv,
+  b.jm_naziv,
+  b.ag_naziv
+FROM 
+  mu_stavke a,
+  vproduct b
+where
+  b.art_id = a.mus_artikal
+order by
+  a.mus_id
